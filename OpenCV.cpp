a2dd6b52bdecd70cd775cba75e7b76eb7922b941
tobiasKoeniger <tobias.koeniger@googlemail.com>
2020-01-01 22:35:58 +0100

    Next commit3

--- OpenCV.cpp
+++ OpenCV.cpp
@@ -1,4 +1,5 @@
 // compile opencv4 with: sudo cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local .. -DOPENCV_GENERATE_PKGCONFIG=ON
+// for compilation see also: https://docs.opencv.org/3.4/d7/d9f/tutorial_linux_install.html
 
 // compile and run with: g++ OpenCV.cpp `pkg-config --cflags --libs opencv4` & ./a.out 
 

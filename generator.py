74276f3fcbcbdee2aa3bef78a75f345a1f0233d9
tobiasKoeniger <tobias.koeniger@googlemail.com>
2020-01-04 17:05:41 +0100

    Next commit3

--- generator.py
+++ generator.py
@@ -2,6 +2,11 @@
 
 # commit and push to git with: git add . ; git commit -m "Next commit3" ; git push origin master
 
+from __future__ import print_function
+from __future__ import division
+from math import atan2, cos, sin, sqrt, pi
+
+
 from os import listdir
 import numpy as np
 
@@ -12,6 +17,8 @@ from mahotas import features
 import math
 
 import cv2
+
+
 
 print("x"*50)
 print("Image Generator")
@@ -82,9 +89,9 @@ print()
 print("Random number between 0 and 10: ")
 print(randint(0, 10))
 
-cv2.imshow('Image 0', pic0)
-cv2.imshow('Image 1', pic1)
-cv2.imshow('Image res', pic_res)
+# cv2.imshow('Image 0', pic0)
+# cv2.imshow('Image 1', pic1)
+# cv2.imshow('Image res', pic_res)
 
 pic_ursprung = pic_res
 
@@ -147,6 +154,91 @@ radius = 200
 zernike = features.zernike_moments(pic_res, radius, ordnung)
 print()
 print("Zernike Momente der Ordnung {}, Radius in px {}: {}".format(ordnung, radius, zernike))
+
+# PCA
+# mean, eigenVectors = cv2.PCACompute(pic_res, mean=None, maxComponents=2)
+
+
+
+def getOrientation(pts, img):
+
+	sz = len(pts)
+	data_pts = np.empty((sz, 2), dtype=np.float64)
+
+	for i in range(data_pts.shape[0]):
+	    data_pts[i,0] = pts[i,0,0]
+	    data_pts[i,1] = pts[i,0,1]
+
+	# Perform PCA analysis
+	mean = np.empty((0))
+	mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
+	# Store the center of the object
+	cntr = (int(mean[0,0]), int(mean[0,1]))
+
+	cv2.circle(img, cntr, 4, (0, 255, 0), 1)
+
+	# p1 = x1, y1
+	# p2 = x2, y2
+	p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
+	p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
+
+	drawAxis(img, cntr, p1, (0, 255, 0), 1)
+	drawAxis(img, cntr, p2, (0, 255, 0), 1)
+
+	print()
+	print("Eigenvectors: {}".format(eigenvectors))
+	print()
+	print("Eigenwerte: {}".format(eigenvalues))
+
+	angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
+
+	return angle
+
+def drawAxis(img, p_, q_, colour, scale):
+    p = list(p_)
+    q = list(q_)
+
+    angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
+    hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+
+    # Here we lengthen the arrow by a factor of scale
+    q[0] = p[0] - scale * hypotenuse * cos(angle)
+    q[1] = p[1] - scale * hypotenuse * sin(angle)
+    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
+
+    # create the arrow hooks
+    p[0] = q[0] + 9 * cos(angle + pi / 4)
+    p[1] = q[1] + 9 * sin(angle + pi / 4)
+    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
+
+    p[0] = q[0] + 9 * cos(angle - pi / 4)
+    p[1] = q[1] + 9 * sin(angle - pi / 4)
+    cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
+
+
+# Convert image to grayscale
+#gray = cv2.cvtColor(pic_res, cv2.COLOR_BGR2GRAY)
+
+# Convert image to binary
+_, bw = cv2.threshold(pic_res, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+## [pre-process]
+
+contours, _ = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+for i, c in enumerate(contours):
+	# Calculate the area of each contour
+	area = cv2.contourArea(c)
+	# Ignore contours that are too small or too large
+	if area < 1e2 or 1e5 < area:
+	    continue
+	# Draw each contour only for visualisation purposes
+	# cv2.drawContours(pic_res, contours, i, (0, 255, 0), 1)
+	# Find the orientation of each shape
+	getOrientation(c, pic_res)
+
+cv2.imshow('output', pic_res)
+print()
+#print(eigenVectors)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()

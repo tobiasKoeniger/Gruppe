05b640ae17472a179d3f5e17841e15a843605ecb
arian171186 <58984935+arian171186@users.noreply.github.com>
2020-01-12 23:33:43 +0100

    Add files via upload

--- generator.py
+++ generator.py
@@ -24,6 +24,7 @@ print("x"*50)
 print()
 
 
+#Funktionsdefinitionen
 
 def invert_image(img):
 	return (255-img)
@@ -55,40 +56,38 @@ def rotate(img, winkel):
 
 
 # Generate Scene
-line_styles = ["dreieck", "ellipse", "gerade", "rechteck"]
-orientierung = ["links", "oben", "rechts", "unten"]
-path0 = ["", "", "", ""]
-path1 = ["", "", "", ""]
+def generateScene():
+    line_styles = ["dreieck", "ellipse", "gerade", "rechteck"]
+    orientierung = ["links", "oben", "rechts", "unten"]
+    path0 = ["", "", "", ""]
+    path1 = ["", "", "", ""]
 
-for num in range(4):
-	path0[num] = "Kanten/" + "kante_" + orientierung[num] + "_" + line_styles[randint(0, 3)] + ".png"
+    for num in range(4):
+        path0[num] = "Kanten/" + "kante_" + orientierung[num] + "_" + line_styles[randint(0, 3)] + ".png"
 
-for num in range(4):
-	path1[num] = "Kanten/" + "kante_" + orientierung[num] + "_" + line_styles[randint(0, 3)] + ".png"
+    for num in range(4):
+        path1[num] = "Kanten/" + "kante_" + orientierung[num] + "_" + line_styles[randint(0, 3)] + ".png"
 
-path1[1] = path0[3]
+    path1[1] = path0[3]
 
-img0 = []
-img1 = []
+    img0 = []
+    img1 = []
 
-for num in range(4):
-	img0.append(cv2.imread(path0[num], 0))
-	img1.append(cv2.imread(path1[num], 0))
+    for num in range(4):
+        img0.append(cv2.imread(path0[num], 0))
+        img1.append(cv2.imread(path1[num], 0))
 
-img1[1] = translate(img1[1], 0, -100)
+    img1[1] = translate(img1[1], 0, -100)
 
-obj0 = add_images4(img0[0], img0[1], img0[2], img0[3])
-obj1 = add_images4(img1[0], img1[1], img1[2], img1[3])
+    obj0 = add_images4(img0[0], img0[1], img0[2], img0[3])
+    obj1 = add_images4(img1[0], img1[1], img1[2], img1[3])
 
-obj1 = translate(obj1, 0, 100)
-
-scene = add_images(obj0, obj1)
-
-cv2.imshow("Scene", scene)
-
+    obj1 = translate(obj1, 0, 100)
+    
+    return add_images(obj0,obj1)
 
 
-def get_huMoments(img):
+def get_huMoments(img, name):
 	# Hu-Momente
 	moments = cv2.moments(img, False)
 	huMoments = cv2.HuMoments(moments)
@@ -97,45 +96,43 @@ def get_huMoments(img):
 	for i in range(0,7):
 		huMoments[i] = -1* math.copysign(1.0, huMoments[i]) * math.log10(abs(huMoments[i]))
 	print()
-	print("HuMoments (log corrected): {}".format(huMoments))
+	print("HuMoments (log corrected) from "+ name +": {}".format(huMoments))
 
 	return huMoments
 
-get_huMoments(scene)
 
-def get_matchShapes(img0, img1):
+def get_matchShapes(img0, img1, name0, name1):
 	# MatchShapes
 	contours_match = [0, 0, 0]
 	contours_match[0] = cv2.matchShapes(img0, img1, cv2.CONTOURS_MATCH_I1, 0)
 	contours_match[1] = cv2.matchShapes(img0, img1, cv2.CONTOURS_MATCH_I2, 0)
 	contours_match[2] = cv2.matchShapes(img0, img1, cv2.CONTOURS_MATCH_I3, 0)
 	print()
-	print("ContoursMatch: {}".format(contours_match))
+	print("ContoursMatch of "+ name0 +" and "+ name1 +": {}".format(contours_match))
 
 	return contours_match
 
 
 # Flächenschwerpunkt berechnen
-moments = cv2.moments(scene, False)
+def calcCentroid(img):
+    moments = cv2.moments(img, False)
 
-cX = int(moments["m10"] / moments["m00"])
-cY = int(moments["m01"] / moments["m00"])
+    cX = float(moments["m10"] / moments["m00"])
+    cY = float(moments["m01"] / moments["m00"])
 
-print()
-print("Flächenschwerpunkt: {}, {}".format(cX, cY))
+    print()
+    print("Flächenschwerpunkt: {}, {}".format(cX, cY))
 
 
 # Zernike Moments
-def get_zernikeMoments(img):
+def get_zernikeMoments(img, name):
 	ordnung = 8
 	radius = 200
 	zernike = features.zernike_moments(img, radius, ordnung)
 	print()
-	print("Zernike Momente der Ordnung {}, Radius in px {}: {}".format(ordnung, radius, zernike))
+	print("Zernike Momente von "+name+" der Ordnung {}, Radius in px {}: {}".format(ordnung, radius, zernike))
 
 	return zernike
-
-get_zernikeMoments(scene)
 
 
 # PCA Achsen Zeichnen
@@ -161,15 +158,41 @@ def drawAxis(img, p_, q_, colour, scale):
     cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), colour, 1, cv2.LINE_AA)
 
 
+#Ausführbereich
 
-scene = rotate(scene, 45)
-scene = translate(scene, 80, -80)
+scene = generateScene()
+#scene2 = generateScene()
 
+cv2.imshow("Scene", scene)
+#cv2.imshow("Scene2", scene2)
+
+get_huMoments(scene, "scene")
+calcCentroid(scene)
+get_zernikeMoments(scene, "Scene")
+
+output1 = rotate(scene, randint(0, 360))
+output1 = translate(output1, randint(-200, 200), randint(-200, 200))
+
+get_huMoments(output1, "output1")
+calcCentroid(output1)
+get_zernikeMoments(output1, "Output1")
+
+output2 = rotate(scene, randint(0, 360))
+output2 = translate(output2, randint(-200, 200), randint(-200, 200))
+
+get_huMoments(output2, "output2")
+calcCentroid(output2)
+get_zernikeMoments(output2, "Output2")
+
+#get_zernikeMoments(scene2, "Scene2")
+
+get_matchShapes(output1, output2, "output1", "output2")
+#get_matchShapes(output1, scene2, "output1", "scene2")
 
 def pca(img):
 
 	# Convert image to binary
-	_, bw = cv2.threshold(scene, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+	_, bw = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
 	contours, _ = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
@@ -222,7 +245,7 @@ def pca(img):
 	# Store the center of the object
 	cntr = (int(mean[0,0]), int(mean[0,1]))
 
-	cv2.circle(scene, cntr, 4, (0, 255, 0), 1)
+	cv2.circle(img, cntr, 4, (0, 255, 0), 1)
 
 	# p1 = x1, y1
 	# p2 = x2, y2
@@ -275,8 +298,8 @@ def pca(img):
 	p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0])
 	p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1])
 
-	drawAxis(scene, cntr, p1, (0, 255, 0), 1)
-	drawAxis(scene, cntr, p2, (0, 255, 0), 1)
+	drawAxis(img, cntr, p1, (0, 255, 0), 1)
+	drawAxis(img, cntr, p2, (0, 255, 0), 1)
 
 	# PCA mit sklearn
 	pca = PCA(n_components=2)
@@ -299,12 +322,14 @@ def pca(img):
 	# drawAxis(scene, cntr, p1, (0, 255, 0), 1)
 	# drawAxis(scene, cntr, p2, (0, 255, 0), 1)
 
-	return scene
+	return img
 
 
-scene = pca(scene)
+output1 = pca(output1)
+output2 = pca(output2)
 
-cv2.imshow('output', scene)
+cv2.imshow('output1', output1)
+cv2.imshow('output2', output2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
